6e4ae23851ea8ac6a4c85c336d4274d4463e5d4e
Kamil <tomiczek.kamil26@gmail.com>
2023-06-07 21:21:58 +0200

    Add calendar nav and clock at the bottom

--- script.js
+++ script.js
@@ -1,17 +1,16 @@
 var months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
 
-createCalendar(new Date())
+var d = new Date()
+var nav = d.getMonth()
 
 function getDays(year, month) {
     return new Date(year, month, 0).getDate();
-};
+}
 
-function createCalendar(d){
-    let st = new Date(d.getFullYear() + "-" + (d.getMonth() + 1) + "-1")
-
+function createCalendar(){
     let calendar = "<tr><th>Mon</th><th>Tue</th><th>Wed</th><th>Thu</th><th>Fri</th><th>Sat</th><th>Sun</th></tr>"
 
-    f = true
+    let f = true
     let l = 0
 
     let daysNumber = getDays(d.getFullYear(), d.getMonth() + 1);
@@ -21,7 +20,7 @@ function createCalendar(d){
         calendar += "<tr>"
 
         if(f){
-            for(let i = 1; i < st.getDay(); i++){
+            for(let i = 1; i < new Date(d.getFullYear(), d.getMonth(), 1).getDay(); i++){
                 calendar += "<td></td>"
                 l++
             }
@@ -53,3 +52,46 @@ function createCalendar(d){
     document.querySelector(".calendar").innerHTML = calendar
     document.querySelector(".month").innerHTML = months[d.getMonth()] + " " + d.getFullYear()
 }
+
+function changeDate(i){
+
+    if(i != 0){
+        if(nav == 0 && i < 0){
+            d.setMonth(nav = 11)
+            d.setFullYear(d.getFullYear() - 1)
+            d.setDate(1)
+        }
+        else if(nav == 11 && i > 0){
+            d.setMonth(nav = 0)
+            d.setFullYear(d.getFullYear() + 1)
+            d.setDate(1)
+        }
+        else {
+            d.setMonth(nav += i)
+            d.setDate(1)
+        }
+    }
+    else{
+        let _now = new Date(Date.now())
+
+        d.setFullYear(_now.getFullYear())
+        d.setMonth(_now.getMonth())
+        d.setDate(_now.getDate())
+
+        nav = _now.getMonth()
+    }
+    
+    createCalendar()
+}
+
+createCalendar()
+
+// Clock
+
+var options = { day: "2-digit", month: "2-digit", year: "numeric", hour: "2-digit", minute: "2-digit", second: "2-digit"}
+const datetimeItem = document.querySelector(".datetime p")
+
+setInterval( () => {
+    var now = new Date(Date.now())
+    datetimeItem.innerHTML = now.toLocaleString("en-US", options)
+}, 1000)
